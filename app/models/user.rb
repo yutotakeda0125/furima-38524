5eaa9f175a2c3c230599a0ad84e29843d0de6973
yutotakeda0125 <yuto-takeda0125@au.com>
2023-02-04 21:26:39 +0900

    userテーブル作成、バリデーション設定

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+   # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true 
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birthday, presence: true
+ 
+  has_many:items
+  has_many:orders
+end
