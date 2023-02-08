74f82dc728ebab02b801294fa4755910b0f6414d
yutotakeda0125 <yuto-takeda0125@au.com>
2023-02-08 22:12:43 +0900

    ユーザー管理機能修正版2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :first_name,format: { with:/\A[ぁ-んァ-ヶ一-龥々]+\z/} 
-  validates :last_name, format: { with:/\A[ぁ-んァ-ヶ一-龥々]+\z/}  
+  validates :first_name,format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/} 
+  validates :last_name, format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/}  
   validates :first_name_kana, format: { with:/\A[ァ-ヶー－]+\z/}
   validates :last_name_kana, format: { with:/\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
