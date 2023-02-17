30cffeececc177af45197f19feb8d95f92138b5b
yutotakeda0125 <yuto-takeda0125@au.com>
2023-02-17 20:33:37 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
-
+  before_action :set_item, only: [:edit,:show,:update]
+  before_action :move_to_index, only: [:edit, :update]
   
    def index
     @items=Item.includes(:user).order("created_at DESC")
@@ -18,17 +19,38 @@ class ItemsController < ApplicationController
       render :new
      end
    end
-   
-    def show
+
+
+    def edit
       @item = Item.find(params[:id])
     end
    
-
-
+    def update
+      @item = Item.find(params[:id])
+     if @item.update(item_params)
+      redirect_to item_path(@item.id)
+     else
+      render :edit
+     end
+    end
+    
+    def show
+      @item = Item.find(params[:id])
+    end
+  
+  
   private
 
   def item_params
     params.require(:item).permit(:name,:description,:category_id,:status_id,:shipping_cost_id,:shipping_date_id,:prefecture_id,:price,:image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
 end
