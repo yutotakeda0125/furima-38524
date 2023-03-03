7aeee4466f7d518b03ebdf4efe120eb61a325185
yutotakeda0125 <yuto-takeda0125@au.com>
2023-03-03 14:57:49 +0900

    フリマ(修正)

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,7 +6,7 @@ class Category < ActiveHash::Base
    { id: 3, name: 'ベビー・キッズ' },
    { id: 4, name: 'インテリア・住まい・小物' },
    { id: 5, name: '本・音楽・ゲーム' },
-   { id: 6, name: 'おもちゃ・ボビー・グッズ' },
+   { id: 6, name: 'おもちゃ・ホビー・グッズ' },
    { id: 7, name: '家電・スマホ・カメラ' },
    { id: 8, name: 'スポーツ・レジャー' },
    { id: 9, name: 'ハンドメイド' },
